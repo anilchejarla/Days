e77b7af246d7c878a452829fc71406b254855b93
anilchejarla <anilchejarla205@gmail.com>
2017-08-03 18:55:24 +0530

    Last modification

--- program.java
+++ program.java
@@ -3,7 +3,7 @@ class addition
 static int a=1000,b=10000,c=a+b;
 public static void main(String []ani)
 {
-System.out.println("The value of c is:...:"+c);
+System.out.println("The value of c is:...Somthing::"+c);
 }
 }
 
